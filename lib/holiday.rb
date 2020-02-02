fc74ec74a69a869130d45602740b1d425fc50389
Timur Mishiev <timurmishiev@gmail.com>
2020-02-02 05:54:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,6 +44,7 @@ def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   holiday_hash.each do | season, holiday|
    puts "#{season.capitalize}:"
+   holiday.each do |holiday, item|
 
 end
 
